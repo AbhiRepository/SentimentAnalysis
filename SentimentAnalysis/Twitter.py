f6fef0b12f178bb9504acc4579aac00d989ea7dd
Abhishek Patel <b516002@iiit-bh.ac.in>
2017-08-31 08:45:35 -1200

    Update Twitter.py

--- SentimentAnalysis/Twitter.py
+++ SentimentAnalysis/Twitter.py
@@ -2,18 +2,18 @@ import tweepy
 import sentiment_mod as s
 
 
-consumer_key = 'Mau4Avgh41GX1Jn5kZ5uvQUWI'
-consumer_secret = 'MLdiQne55btJIL5z1vEFRaSeHhwkLUfaGPqO1YPQTtCdiSZA12'
+consumer_key = '****'
+consumer_secret = '***'
 
-access_token = '902339453158436865-KMVKojyAcEGSErOYgc2LmnwJ1lGLLZt'
-access_token_secret = '2a4g3o55LNYVB7peF3WxlqPZwfuE53iPhUcxRnxa1RChR'
+access_token = '****'
+access_token_secret = '***'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.search('Annabelle: Creation')
+public_tweets = api.search('*** Anytopic you like****')
 
 for tweet in public_tweets:
 	print(tweet.text)
