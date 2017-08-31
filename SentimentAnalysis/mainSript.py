4715c0ff21c020ec45967ad704b6e0fc3a7f29b0
Abhishek Patel <b516002@iiit-bh.ac.in>
2017-08-30 19:59:26 -1200

    Add files via upload

---
+++ SentimentAnalysis/mainSript.py
@@ -0,0 +1,3 @@
+import sentiment_mod as s
+
+print(s.sentiment("Wasn't as bad as I'd expected. Unlike I'd feared, its long runtime did not work as a spoilsport and I was hardly distracted/irritated throughout the film. While it's not an incredible flick, it's quite watchable."))
